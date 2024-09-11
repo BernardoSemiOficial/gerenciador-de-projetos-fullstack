085292fa1e74da8340e398648e7fac0478798692
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-09-11 20:31:17 -0300

    feat: criado formulários de login e registro

---
+++ frontend/src/app/features/login/login.component.ts
@@ -0,0 +1,63 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import {
+  LoginPayload,
+  LoginResponse,
+} from '@core/interfaces/authentication.interface';
+import { AuthService } from '@core/services/auth/auth.service';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, InputTextModule, PasswordModule, ButtonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  private formBuilder: FormBuilder = inject(FormBuilder);
+  private authService: AuthService = inject(AuthService);
+  private messageService: MessageService = inject(MessageService);
+  private router: Router = inject(Router);
+  loginForm = this.initForm();
+
+  initForm() {
+    return this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  loginUser() {
+    if (this.loginForm.invalid) return;
+
+    const registerFormValue = this.loginForm.getRawValue();
+    const payload: LoginPayload = {
+      email: registerFormValue.email!,
+      password: registerFormValue.password!,
+    };
+    this.authService.login(payload).subscribe({
+      next: (data: LoginResponse) => {
+        this.authService.setTokens(data);
+        this.router.navigate(['/dashboard']);
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Success',
+          detail: 'User logged in',
+        });
+      },
+      error: (error: HttpErrorResponse) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Invalid credentials',
+        });
+      },
+    });
+  }
+}
---
+++ frontend/src/app/features/register/register.component.ts
@@ -0,0 +1,62 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegisterPayload } from '@core/interfaces/authentication.interface';
+import { AuthService } from '@core/services/auth/auth.service';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [ReactiveFormsModule, InputTextModule, PasswordModule, ButtonModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent {
+  private formBuilder: FormBuilder = inject(FormBuilder);
+  private authService: AuthService = inject(AuthService);
+  private messageService: MessageService = inject(MessageService);
+  private router: Router = inject(Router);
+  registerForm = this.initForm();
+
+  initForm() {
+    return this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  registerUser() {
+    if (this.registerForm.invalid) return;
+
+    const registerFormValue = this.registerForm.getRawValue();
+    const payload: RegisterPayload = {
+      name: registerFormValue.name!,
+      email: registerFormValue.email!,
+      password: registerFormValue.password!,
+    };
+    this.authService.register(payload).subscribe({
+      next: () => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Success',
+          detail: 'User registered',
+        });
+        this.router.navigate(['/login']);
+      },
+      error: (error: HttpErrorResponse) => {
+        console.log(error);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'User registration failed',
+        });
+      },
+    });
+  }
+}
