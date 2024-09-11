06dc41549461a024bd47599524715a65c5beca54
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-09-11 18:05:57 -0300

    chore: criado projeto frontend e configurado lib de componentes

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { CalendarModule } from 'primeng/calendar';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CalendarModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = '@gerenciador-de-tarefas/frontend';
+  date1: Date | undefined;
+}
