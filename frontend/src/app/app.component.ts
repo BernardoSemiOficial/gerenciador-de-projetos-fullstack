d70459d6a0721fe64acf3765c1bccb739044156f
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-09-11 20:34:32 -0300

    chore: adicionado serviço Message e criado enum

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, CalendarModule, FormsModule],
+  providers: [MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ frontend/src/app/core/enums/local-storage.enum.ts
@@ -0,0 +1,4 @@
+export enum LocalStorage {
+  AccessToken = '@AUTH_ACCESS_TOKEN',
+  RefreshToken = '@AUTH_REFRESH_TOKEN',
+}
