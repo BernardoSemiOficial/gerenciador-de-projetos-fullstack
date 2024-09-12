4001ac867295fe8c374731707e644c929bf5cebd
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-09-12 16:30:36 -0300

    feat: criado header para o projeto

--- frontend/src/app/shared/app-header/app-header.component.ts
+++ frontend/src/app/shared/app-header/app-header.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '@core/services/auth/auth.service';
+import { UserService } from '@core/services/user/user.service';
+import { MenuItem, PrimeIcons } from 'primeng/api';
+import { AvatarModule } from 'primeng/avatar';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, MenubarModule, AvatarModule],
   templateUrl: './app-header.component.html',
   styleUrl: './app-header.component.scss',
 })
-export class AppHeaderComponent {}
+export class AppHeaderComponent implements OnInit {
+  userService = inject(UserService);
+  authService = inject(AuthService);
+  user = this.userService.user();
+  items: MenuItem[] | undefined;
+  labelAvatar = '';
+  PrimeIcons = PrimeIcons;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: PrimeIcons.HOME,
+        routerLink: ['/dashboard'],
+      },
+      {
+        label: 'Logout',
+        icon: PrimeIcons.SIGN_OUT,
+      },
+    ];
+    this.formatLabelAvatar();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+  formatLabelAvatar() {
+    this.labelAvatar = this.user?.name?.charAt(0).toUpperCase() ?? '@';
+  }
+}
