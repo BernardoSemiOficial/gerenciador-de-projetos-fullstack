b1dfee739e6755d59a5a4ed8d5dbf0f5a26a8ef5
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-09-14 10:45:08 -0300

    feat: detalhado página de projeto

--- frontend/src/app/features/projects/view-project/view-project.component.ts
+++ frontend/src/app/features/projects/view-project/view-project.component.ts
@@ -1,25 +1,38 @@
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
 import { Component, inject, input, OnInit } from '@angular/core';
 import { ProjectService } from '@core/services/project/project.service';
 import { ResponseGetProject } from '@core/services/project/project.service.types';
+import { AppChipComponent } from '@shared/app-chip/app-chip.component';
+import { AppDividerComponent } from '@shared/app-divider/app-divider.component';
 import { AppFooterComponent } from '@shared/app-footer/app-footer.component';
 import { AppHeaderComponent } from '@shared/app-header/app-header.component';
+import { PrimeIcons } from 'primeng/api';
 import { Observable } from 'rxjs';
+import { AppCardComponent } from '../../../shared/app-card/app-card.component';
 
 @Component({
   selector: 'app-view-project',
   standalone: true,
-  imports: [AppHeaderComponent, AppFooterComponent, AsyncPipe, JsonPipe],
+  imports: [
+    AppHeaderComponent,
+    AppFooterComponent,
+    AppCardComponent,
+    AppChipComponent,
+    AppDividerComponent,
+    AsyncPipe,
+    DatePipe,
+  ],
   templateUrl: './view-project.component.html',
   styleUrl: './view-project.component.scss',
 })
 export class ViewProjectComponent implements OnInit {
   projectService = inject(ProjectService);
   projectId = input.required<string>();
+  PrimeIcons = PrimeIcons;
 
-  project$!: Observable<ResponseGetProject>;
+  getProject$!: Observable<ResponseGetProject>;
 
   ngOnInit() {
-    this.project$ = this.projectService.getProject(this.projectId());
+    this.getProject$ = this.projectService.getProject(this.projectId());
   }
 }
