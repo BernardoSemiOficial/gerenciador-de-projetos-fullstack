739a3637ba3281ded33cbccf9b310ca3fd9d1657
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-09-14 11:32:38 -0300

    feat: utilizado componente de criar e editar projeto em página

--- frontend/src/app/pages/projects-page/create-project-page/create-project-page.component.ts
+++ frontend/src/app/pages/projects-page/create-project-page/create-project-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { CreateProjectComponent } from '@features/projects/create-project/create-project.component';
+import { CreateEditProjectComponent } from '@features/projects/create-edit-project/create-edit-project.component';
 
 @Component({
   selector: 'app-create-project-page',
   standalone: true,
-  imports: [CreateProjectComponent],
+  imports: [CreateEditProjectComponent],
   templateUrl: './create-project-page.component.html',
   styleUrl: './create-project-page.component.scss',
 })
