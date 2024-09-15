c809195fcbca4a37ae17925b9792e4f6e50af97d
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-09-15 17:25:40 -0300

    feat: adicionado link entre as página de login e registro

--- frontend/src/app/pages/login-page/login-page.component.ts
+++ frontend/src/app/pages/login-page/login-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { LoginComponent } from '@features/login/login.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [LoginComponent],
+  imports: [LoginComponent, RouterLink],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
--- frontend/src/app/pages/register-page/register-page.component.ts
+++ frontend/src/app/pages/register-page/register-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { RegisterComponent } from '@features/register/register.component';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [RegisterComponent],
+  imports: [RegisterComponent, RouterLink],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss',
 })
