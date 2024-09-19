994c357ed020d215a812412131b792988018ecb5
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-09-18 23:34:43 -0300

    feat: criado componente de mult select

---
+++ frontend/src/app/shared/app-multi-select/app-multi-select.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppMultiSelectComponent } from './app-multi-select.component';
+
+describe('AppMultiSelectComponent', () => {
+  let component: AppMultiSelectComponent;
+  let fixture: ComponentFixture<AppMultiSelectComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppMultiSelectComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AppMultiSelectComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/shared/app-multi-select/app-multi-select.component.ts
@@ -0,0 +1,48 @@
+import { Component, forwardRef, input } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormsModule,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+import { MultiSelectModule } from 'primeng/multiselect';
+
+@Component({
+  selector: 'app-multi-select',
+  standalone: true,
+  imports: [MultiSelectModule, FormsModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => AppMultiSelectComponent),
+      multi: true,
+    },
+  ],
+  templateUrl: './app-multi-select.component.html',
+  styleUrl: './app-multi-select.component.scss',
+})
+export class AppMultiSelectComponent implements ControlValueAccessor {
+  options = input.required<any[]>();
+  id = input.required<string>();
+  inputType = input<HTMLInputElement['type']>('text');
+  placeholder = input<HTMLInputElement['placeholder']>();
+  value: any;
+  isDisabled: boolean = false;
+
+  onChange = (_: any) => {};
+  onTouched = () => {};
+
+  writeValue(value: any): void {
+    if (value !== this.value) {
+      this.value = value;
+    }
+  }
+  registerOnChange(fn: (_: any) => void): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+}
