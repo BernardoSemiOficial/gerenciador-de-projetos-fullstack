c60042ae9abdb3cdb224f6fbb21693b9c3d92dde
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-09-19 12:55:47 -0300

    feat: adicionado o componente de dashboard dialog invites

--- frontend/src/app/features/dashboard/dashboard.component.ts
+++ frontend/src/app/features/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, effect, inject } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ProjectForUser } from '@core/interfaces/user.interface';
 import { UserService } from '@core/services/user/user.service';
 import { ResponseGetProjectsForUser } from '@core/services/user/user.service.types';
 import { AppButtonComponent } from '@shared/app-button/app-button.component';
@@ -8,7 +10,8 @@ import { AppCardComponent } from '@shared/app-card/app-card.component';
 import { AppFooterComponent } from '@shared/app-footer/app-footer.component';
 import { AppHeaderComponent } from '@shared/app-header/app-header.component';
 import { PrimeIcons } from 'primeng/api';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { DashboardDialogInvitesComponent } from './components/dashboard-dialog-invites/dashboard-dialog-invites.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -16,10 +19,12 @@ import { Observable } from 'rxjs';
   imports: [
     CommonModule,
     RouterModule,
+    ReactiveFormsModule,
     AppHeaderComponent,
     AppFooterComponent,
     AppCardComponent,
     AppButtonComponent,
+    DashboardDialogInvitesComponent,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
@@ -29,12 +34,25 @@ export class DashboardComponent {
   user = this.userService.user();
   userProjects$!: Observable<ResponseGetProjectsForUser>;
   PrimeIcons = PrimeIcons;
+  viewDialogInvites = signal<boolean>(true);
+  projects: ProjectForUser[] = [];
 
   constructor() {
     effect(() => {
       if (this.user) {
-        this.userProjects$ = this.userService.getProjectsForUser(this.user?.id);
+        this.userProjects$ = this.userService
+          .getProjectsForUser(this.user?.id)
+          .pipe(
+            map((data) => {
+              this.projects = data.projects;
+              return data;
+            })
+          );
       }
     });
+  }
+
+  showDialogInvites() {
+    this.viewDialogInvites.set(true);
   }
 }
